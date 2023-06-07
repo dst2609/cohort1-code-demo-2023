fdd16d339cb15cfdcaaadd6e020aa37770214722
Devarsh Thaker <devarsht@gmail.com>
2023-06-07 12:00:44 -0700

    uploaded w1d3 live coding example.

---
+++ week1/w1d3/script.js
@@ -0,0 +1,24 @@
+//Modifying an element
+let title = document.querySelector("#title");
+title.innerHTML = "Cohort 1 is Awesome!!!";
+// title.innerHTML = "<h6>HELLLOOOO</h6>";
+
+//Adding an element
+let addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", function () {
+  let newItem = document.createElement("li");
+  newItem.textContent = "New Item";
+
+  let list = document.querySelector("#list");
+  list.appendChild(newItem);
+});
+
+//removing an element
+let removeBtn = document.querySelector("#removeBtn");
+removeBtn.addEventListener("click", function () {
+  let list = document.querySelector("#list");
+  let lastItem = list.lastElementChild;
+  if (lastItem) {
+    list.removeChild(lastItem);
+  }
+});
