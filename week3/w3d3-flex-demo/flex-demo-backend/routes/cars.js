0ff73601aefedbc91c9d2612a5e1dc268a4a32fa
Devarsh Thaker <devarsht@gmail.com>
2023-06-22 09:41:01 -0700

    added error handling in routes

--- week3/w3d3-flex-demo/flex-demo-backend/routes/cars.js
+++ week3/w3d3-flex-demo/flex-demo-backend/routes/cars.js
@@ -9,22 +9,56 @@ router.get("/", (req, res) => {
   res.json(cars);
 });
 
-//route to get car by id
+// //route to get car by id
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const car = carModel.getCarById(parseInt(id));
+//   if (!car) {
+//     return res.status(404).json({ message: "Car not found" });
+//   }
+//   res.json(car);
+// });
+
+// //route to add new car
+// router.post("/", (req, res) => {
+//   const { brand, model, year } = req.body;
+//   const newCar = carModel.addCar({ brand, model, year });
+//   res.status(201).json(newCar);
+// });
+
+//Error handling in routes
 router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const car = carModel.getCarById(parseInt(id));
-  if (!car) {
-    return res.status(404).json({ message: "Car not found" });
+  try {
+    const { id } = req.params;
+    const car = carModel.getCarById(parseInt(id));
+    if (!car) {
+      return res
+        .status(404)
+        .json({ message: "Car not found in the Cars array" });
+    }
+    res.json(car);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
   }
-  res.json(car);
 });
 
-//route to add new car
+// Create a new car
 router.post("/", (req, res) => {
-  const { brand, model, year } = req.body;
-  const newCar = carModel.addCar({ brand, model, year });
-  res.status(201).json(newCar);
+  try {
+    const { brand, model, year } = req.body;
+    if (!brand || !model || !year) {
+      return res
+        .status(400)
+        .json({ message: "Please provide brand, model, and year" });
+    }
+    const newCar = carModel.addCar({ brand, model, year });
+    res.status(201).json(newCar);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 });
+
+//
 
 //route to update car
 router.put("/:id", (req, res) => {
